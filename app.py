08cd67b5717f1ece392adf407a124bc7730a35bb
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-09-30 14:02:20 +0530

    final commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template, request
+import csv
 
 app = Flask(__name__)
 
@@ -7,10 +8,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+def convert_to_csv(data):
+    with open('database.csv',newline='', mode='a') as database:
+        username = data['user_name']
+        email = data['email']
+        subject = data['subject']
+        contents = data['contents']
+        data_Writer = csv.writer(database, delimiter='|')
+        data_Writer.writerow([username, email, subject, contents])
+
 @app.route('/submit_form', methods=['POST','GET'])
 def submit():
     if request.method == 'POST':
         data = request.form.to_dict()
+        convert_to_csv(data)
         return render_template('thankyou.html', context=data)
     else:
         return 'Something went wrong , Buddy check out !!'
