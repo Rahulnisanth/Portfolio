d2686c5afb6cbfb137f41798a9a59dcbb1055770
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-12-17 19:57:14 +0530

    added-projects

--- app.py
+++ app.py
@@ -4,24 +4,27 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 def convert_to_csv(data):
-    with open('database.csv',newline='', mode='a') as database:
-        username = data['user_name']
-        email = data['email']
-        subject = data['subject']
-        contents = data['contents']
-        data_Writer = csv.writer(database, delimiter='|')
+    with open("database.csv", newline="", mode="a") as database:
+        username = data["user_name"]
+        email = data["email"]
+        subject = data["subject"]
+        contents = data["contents"]
+        data_Writer = csv.writer(database, delimiter="|")
         data_Writer.writerow([username, email, subject, contents])
 
-@app.route('/submit_form', methods=['POST','GET'])
+
+@app.route("/submit_form", methods=["POST", "GET"])
 def submit():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.form.to_dict()
         convert_to_csv(data)
-        return render_template('thankyou.html', context=data)
+        return render_template("thankyou.html", context=data)
     else:
-        return 'Something went wrong , Buddy check out !!'
+        return "Something went wrong , Buddy check out !!"
